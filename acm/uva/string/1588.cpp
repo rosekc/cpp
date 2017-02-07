b822d2821ca936b1041eb5fbe3f105aba0c4e317
rosekc <rose-kc@hotmail.com>
2017-02-07 18:28:40 +0800

    finish uva 1588 cf 170202 d

--- acm/uva/string/1588.cpp
+++ acm/uva/string/1588.cpp
@@ -5,42 +5,39 @@
 using namespace std;
 
 char d1[105], d2[105];
+int len1, len2;
+bool check(int i, int j)
+{
+    int i1 = i,  j1 = j;
+    while (i1 < len1 && j1 < len2)
+    {
+        if (d1[i1++] == '2' && d2[j1] == '2')
+        {
+            //printf("  %d %d\n", i1, j1);
+            return 0;
+        }
+        j1++;
+    }
+    //printf("%d %d\n", i1, j1);
+    return 1;
+}
 
 int main()
 {
     while (scanf("%s%s", d1, d2) != EOF)
     {
-        int len1 = strlen(d1);
-        int len2 = strlen(d2);
-        int re = -1;
+        len1 = strlen(d1);
+        len2 = strlen(d2);
+        int re = 2000;
         for (int i = 0; i < len1; i++)
         {
-            bool f1 = 1, f2 = 1;
-            for (int j = 0; j < len2 && i + j < len1; j++)
-            {
-                if (d1[i + j] == '2' && d2[j] == '2')
-                {
-                    printf("%d %d\n", i + j, j);
-                    f1 = 0;
-                    break;
-                }
-            }
-            for (int j = len2 - 1; i + len2 - 1 - j >= 0 && i + len2 - 1 - j < len1; j--)
-            {
-                if (d1[i + len2 - 1 - j] == '2' && d2[j] == '2')
-                {
-                    f2 = 0;
-                    break;
-                }
-            }
-            if (f1 || f2)
-            {
-                re = max(i + len2, len1);
-                printf("%d %d\n", re, i);
-                break;
-            }
+            if (check(i, 0)) re = min(re, max(i + len2, len1));
         }
-        if (re == -1) re = len1 + len2;
+        for (int i = 0; i < len2; i++)
+        {
+            if (check(0, i)) re = min(re, max(i + len1, len2));
+        }
+        re = min(len2 + len1, re);
         printf("%d\n", re);
     }
 }
