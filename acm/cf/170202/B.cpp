58760571da7603a7da398991a75c9129c1089710
rosekc <rose-kc@hotmail.com>
2017-02-15 23:39:01 +0800

    update some problem

--- acm/cf/170202/B.cpp
+++ acm/cf/170202/B.cpp
@@ -1,5 +1,5 @@
 //2017-02-02-21.49
-//
+//B
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,20 +11,13 @@ int main()
     int n;
     scanf("%d", &n);
     for (int i = 0; i < n; i++) scanf("%d", &d[i]);
-    int b = n - 1;
-    int f = 0;
+    for (int i = 0; i < n / 2; i++)
+    {
+        if (i % 2 == 0) swap(d[i], d[n - i - 1]);
+    }
     for (int i = 0; i < n; i++)
     {
-        if (n % 2 == 0 && i == n / 2) f = 1, b--;
-        if ((i + f) % 2 == 0)
-        {
-            printf("%d ", d[b]);
-            b -= 2;
-        }
-        else
-        {
-            printf("%d ", d[i]);
-        }
+        printf("%d ", d[i]);
     }
     puts("");
 }
--- acm/uva/ch04/213.cpp
+++ acm/uva/ch04/213.cpp
@@ -1,29 +1,41 @@
 //2017-02-11-23.16
-//Uva
+//Uva 213
 
 #include <bits/stdc++.h>
 using namespace std;
 
 int readchar()
 {
-    char ch = getchar();
-    while (ch == '\n' || ch == '\r') ch = getchar();
+    char ch;
+    do ch = getchar();
+    while (ch == '\n' || ch == '\r');
     return ch;
 }
 
 int readint(int c)
 {
-    w = 0;
+    int w = 0;
     while (c--)
     {
-        w = readchar() - '0' + w << 1;
+        w = readchar() - '0' + (w << 1);
     }
+    return w;
 }
 
 int code[8][1 << 8];
 int readcodes()
 {
-
+    memset(code, 0, sizeof code);
+    for (int len = 1; len <= 7; len++)
+        for (int i = 0; i < (1 << len) - 1; i++)
+    {
+        int ch;
+        if (len != 1) ch = getchar();
+        else ch = readchar();
+        if (ch == EOF) return 0;
+        if (ch == '\n' || ch == '\r') return 1;
+        code[len][i] = ch;
+    }
 }
 int main()
 {
